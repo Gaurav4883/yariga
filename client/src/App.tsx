383a901bc195fa6f7158152bd4f31bc7155ce43a
gaurav4883 <gaurav.deol1600@gmail.com>
2023-02-22 23:21:34 +0545

    added live backend link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -58,7 +58,7 @@ function App() {
 
       if (profileObj) {
         const response = await fetch(
-          "http://localhost:8080/api/v1/users",
+          "https://yariga-85kx.onrender.com/api/v1/users",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -128,7 +128,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://yariga-85kx.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
